bada8c494a62a00b5f9efbf82d567a9c8b2124be
rupakkatwal <rupak2072@gmail.com>
2022-05-25 12:51:01 +0200

    fix err

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { MsalAuthenticationTemplate, useMsal } from "@azure/msal-react";
 import { InteractionType } from "@azure/msal-browser";
